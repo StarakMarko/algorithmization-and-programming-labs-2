4228af2b79be29a57e8c6da38c2eed063b3dba70
StarakMarko <marko.starak.ir.2024@lpnu.ua>
2025-02-28 20:41:01 +0200

    add lab_1

---
+++ lab_1.py
@@ -0,0 +1,53 @@
+# В3 P3
+import unittest
+
+lst = [1, 3, 5, 4, 2, 8, 3, 7]
+
+
+def peak_sequence(lst):
+    max_length = 0
+    i = 1
+    for i in range(1, len(lst) - 1):
+        if lst[i - 1] < lst[i] and lst[i] > lst[i + 1]:
+            left_index = i - 1
+            left_len = 1
+            right_index = i + 1
+            right_len = 1
+            while left_index > 0 and lst[left_index - 1] < lst[left_index]:
+                left_index -= 1
+                left_len += 1
+            while (
+                right_index < len(lst) - 1 and lst[right_index] > lst[right_index + 1]
+            ):
+                right_index += 1
+                right_len += 1
+            length = 0
+            if left_len > right_len:
+                length = right_len * 2 + 1
+            else:
+                length = left_len * 2 + 1
+            if max_length < length:
+                max_length = length
+    return max_length
+
+
+print(peak_sequence(lst))
+
+
+class TestFunc(unittest.TestCase):
+    def test_sorted_increasing(self):
+        increasing_lst = [i for i in range(10)]
+        self.assertEqual(peak_sequence(increasing_lst), 0)
+
+    def test_sorted_decreasing(self):
+        decreasing_lst = [i for i in range(10, -1)]
+        self.assertEqual(peak_sequence(decreasing_lst), 0)
+
+    def test_two_elements(self):
+        self.assertEqual(peak_sequence([1, 2]), 0)
+
+    def test_three_peaks(self):
+        self.assertEqual(peak_sequence([1, 3, 5, 4, 2, 8, 3, 7, 4]), 5)
+
+
+unittest.main()
