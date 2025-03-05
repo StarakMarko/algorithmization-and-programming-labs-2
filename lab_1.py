005432278c2dc11b6cbacaf14c9e0afd1fe62ad2
StarakMarko <marko.starak.ir.2024@lpnu.ua>
2025-03-05 17:08:14 +0200

    2 commit

--- lab_1.py
+++ lab_1.py
@@ -1,5 +1,4 @@
 # В3 P3
-import unittest
 
 lst = [1, 3, 5, 4, 2, 8, 3, 7]
 
@@ -32,22 +31,3 @@ def peak_sequence(lst):
 
 
 print(peak_sequence(lst))
-
-
-class TestFunc(unittest.TestCase):
-    def test_sorted_increasing(self):
-        increasing_lst = [i for i in range(10)]
-        self.assertEqual(peak_sequence(increasing_lst), 0)
-
-    def test_sorted_decreasing(self):
-        decreasing_lst = [i for i in range(10, -1)]
-        self.assertEqual(peak_sequence(decreasing_lst), 0)
-
-    def test_two_elements(self):
-        self.assertEqual(peak_sequence([1, 2]), 0)
-
-    def test_three_peaks(self):
-        self.assertEqual(peak_sequence([1, 3, 5, 4, 2, 8, 3, 7, 4]), 5)
-
-
-unittest.main()
